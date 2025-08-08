252987531f14e2ca92d8bee513da49178058381b
SIDDARTHA GALLA <siddarthagalla@gmail.com>
2025-08-08 23:02:00 +0530

    Create TREE_TRAVERSALS.C

---
+++ TREE_TRAVERSALS.C
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left, *right;
+};
+
+struct Node* create(int val) {
+    struct Node* n = (struct Node*) malloc(sizeof(struct Node));
+    n->data = val;
+    n->left = n->right = NULL;
+    return n;
+}
+
+void inorder(struct Node* root) {
+    if(root) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void preorder(struct Node* root) {
+    if(root) {
+        printf("%d ", root->data);
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(struct Node* root) {
+    if(root) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main() {
+    struct Node* root = create(17);
+    root->left = create(31);
+    root->right = create(23);
+
+    printf("Inorder: "); inorder(root);
+    printf("\nPreorder: "); preorder(root);
+    printf("\nPostorder: "); postorder(root);
+    return 0;
+}
+
+OUTPUT :
+Inorder: 31 17 23 
+Preorder: 17 31 23 
+Postorder: 31 23 17
