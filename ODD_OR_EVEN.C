7cfdaad5441cca714a69d26593d15af3feae001b
SIDDARTHA GALLA <siddarthagalla@gmail.com>
2025-08-08 22:35:00 +0530

    Create ODD_OR_EVEN.C

---
+++ ODD_OR_EVEN.C
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, number;
+
+    printf("Enter how many numbers you want to check: ");
+    scanf("%d", &n);
+
+    printf("Enter %d numbers:\n", n);
+    for(i = 1; i <= n; i++) {
+        scanf("%d", &number);
+
+        if(number % 2 == 0)
+            printf("Number %d is Even\n", number);
+        else
+            printf("Number %d is Odd\n", number);
+    }
+
+    return 0;
+}
+
+OUTPUT :
+Enter 5 numbers:
+10 20 30 40 50
+Number 10 is Even
+Number 20 is Even
+Number 30 is Even
+Number 40 is Even
+Number 50 is Even
